657ff84fc5103426e2f5f7f767139ebf9dc7134d
Romain Le Quellec <romain.lequellec-ext@elis.com>
2018-12-14 13:47:30 +0100

    format sol 001 & 002

--- 002_even_fibonacci_numbers/solution.js
+++ 002_even_fibonacci_numbers/solution.js
@@ -1,12 +1,12 @@
 let sum = 0;
 var prev = 1;
 var current = 2;
-let tmp=0;
+let tmp = 0;
 let fibonacci = () => {
-  tmp = current
-  current += prev
-  prev = tmp
-}
+  tmp = current;
+  current += prev;
+  prev = tmp;
+};
 
 while (current < 4000000) {
   if (current % 2 === 0) {
